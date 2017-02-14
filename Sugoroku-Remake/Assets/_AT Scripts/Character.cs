226a4f8b32c1a4d87943a1df211e9a40e0727698
AllTradesGames <alltradesgames@gmail.com>
2017-02-14 13:26:15 -0700

    (1) Design/Create Data Models [0.8]
    Created "Item" and "Character" data models.

---
+++ Sugoroku-Remake/Assets/_AT Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Character
+{
+    public string name;
+    public int maxHP;
+    public int tempMaxHP;
+    public int currentHP;
+    public int level;
+    public int pointsAvailable;
+    public int hpPoints;
+    public int movementPoints;
+    public int attackPoints;
+    public int defensePoints;
+    public int movementBonus;
+    public int attackBonus;
+    public int defenseBonus;
+    public int credits;
+    public List<Item> itemList;
+
+    public Character()
+    {
+        maxHP = 10;
+        tempMaxHP = 10;
+        currentHP = 10;
+        level = 1;
+        pointsAvailable = 11;
+        hpPoints = 1;
+        movementPoints = 1;
+        attackPoints = 1;
+        defensePoints = 1;
+        movementBonus = 0;
+        attackBonus = 1;
+        defenseBonus = 0;
+        credits = 0;
+        itemList = new List<Item>();
+    }
+
+}
---
+++ Sugoroku-Remake/Assets/_AT Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+[Serializable]
+public class Item
+{
+    public string name;
+    public string imageName;
+    public int basePrice;
+    public int pricePerLevel;
+    public int itemEffect;
+    public int effectAmount;
+    public bool isIdentified;
+
+    public Item()
+    {
+        
+    }
+
+}
